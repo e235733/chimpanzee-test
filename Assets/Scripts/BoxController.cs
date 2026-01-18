a68279faa4d1a45ee4900cb2d6216ae7866a944a
e235733 <e235733@ie.u-ryukyu.ac.jp>
2026-01-18 22:11:18 +0900

    feat: implement NumberBox and controlling show/hide

---
+++ Assets/Scripts/BoxController.cs
@@ -0,0 +1,38 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoxController : MonoBehaviour
+{
+    [Header("UI 参照")]
+    [SerializeField] private Image boxImage;
+    [SerializeField] private TextMeshProUGUI numberText;
+
+    private float timeCounter = 0f;
+
+    private void Update()
+    {
+        timeCounter += Time.deltaTime;
+
+        if (timeCounter > 0.5)
+        {
+            ToggleVisibility();
+            timeCounter = 0;
+        }
+    }
+
+    // 数字の設定
+    public void SetNumber(int number)
+    {
+        numberText.text = number.ToString();
+        boxImage.gameObject.SetActive(false);
+    }
+
+    // 表示・非表示を切り替えるトグルメソッド
+    public void ToggleVisibility()
+    {
+        // 現在の状態を調べ、反転させる
+        bool isBoxVisibly = boxImage.gameObject.activeSelf;
+        boxImage.gameObject.SetActive(!isBoxVisibly);
+    }
+}
