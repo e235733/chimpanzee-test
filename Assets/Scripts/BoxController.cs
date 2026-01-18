b5efd2a6655989d4d9aafbd65bc8bc648665f1ea
e235733 <e235733@ie.u-ryukyu.ac.jp>
2026-01-18 22:42:25 +0900

    fix: save forgotten change

--- Assets/Scripts/BoxController.cs
+++ Assets/Scripts/BoxController.cs
@@ -8,6 +8,7 @@ public class BoxController : MonoBehaviour
     [SerializeField] private Image boxImage;
     [SerializeField] private TextMeshProUGUI numberText;
 
+
     private void Update()
     {
         
