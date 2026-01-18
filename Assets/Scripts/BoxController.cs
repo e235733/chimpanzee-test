6df599c6e8fef16957659a33f36d08ac642010c5
e235733 <e235733@ie.u-ryukyu.ac.jp>
2026-01-19 01:52:51 +0900

    feat: implement prefab and instantiate method

--- Assets/Scripts/BoxController.cs
+++ Assets/Scripts/BoxController.cs
@@ -1,14 +1,23 @@
-using Microsoft.Unity.VisualStudio.Editor;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoxController : MonoBehaviour
 {
-    [SerializeField] private GameManager gameManager;
     [SerializeField] private Image boxImage;
     [SerializeField] private TextMeshProUGUI numberText;
 
     private int number = 0;
+    private GameManager gameManager;
+
+    // instantiate 時に呼び出す設定
+    public void Setup(int _number, GameManager _gameManager)
+    {
+        number = _number;
+        gameManager = _gameManager;
+
+        numberText.text = number.ToString();
+    }
 
     public void OnClickBox()
     {
